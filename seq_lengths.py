0c38eea26a3c0f1764339bc015f54bc33d4b4db3
Lex Nederbragt <lex.nederbragt@ibv.uio.no>
2021-11-16 15:11:00 +0100

    Added comments

--- seq_lengths.py
+++ seq_lengths.py
@@ -4,9 +4,13 @@ with open(sys.argv[1], "r") as fh:
     lines = fh.readlines()
 
 for line in lines:
+    # remove newlines
     line = line.strip()
+    # header line starts with ">"
     if line.startswith(">"):
         header = line
+    # all other lines are sequences
     else:
         length = len(line)
+        # print results
         print(length, header)
